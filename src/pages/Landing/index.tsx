e4724b9badfd22af3fe09a79cbb710776b7de8cb
Wellington Perez <wellingtonx3@hotmail.com>
2020-08-04 15:49:38 -0300

    Add routes

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -8,6 +8,7 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 
 import './styles.css'
+import { Link } from 'react-router-dom'
 
 function Landing() {
   return (
@@ -26,15 +27,15 @@ function Landing() {
         />
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar"/>
             Estudar
-          </a>
+          </Link>
 
-          <a href="" className="give-classes">
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Dar aulas"/>
             Dar aulas
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TeacherList() {
+  return (
+    <div>
+      <h1>Study</h1>
+    </div>
+  )
+}
+
+export default TeacherList
