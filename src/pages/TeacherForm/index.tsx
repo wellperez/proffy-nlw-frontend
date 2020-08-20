95ab04d576cebb013db1e46a446cbff11911402f
Wellington Perez <wellingtonaperez@gmail.com>
2020-08-20 12:33:08 -0300

    Add select component

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import PageHeader from '../../components/PageHeader'
 import Input from '../../components/Input'
+import Textarea from '../../components/Textarea'
+import Select from '../../components/Select'
 
 import warningIcon from '../../assets/images/icons/warning.svg'
 
 import './styles.css'
-import Textarea from '../../components/Textarea'
+
 
 
 function TeacherForm() {
@@ -29,7 +31,22 @@ function TeacherForm() {
           <fieldset>
             <legend>Sobre a aula</legend>
             
-            <Input name="subject" label="Matéria"></Input>
+            <Select
+              name="subject"
+              label="Matéria"
+              options={[
+                { value: "Artes", label:"Artes" },
+                { value: "Biologia", label:"Biologia" },
+                { value: "Ciências", label:"Ciências" },
+                { value: "Educação Física", label:"Educação Física" },
+                { value: "Física", label:"Física" },
+                { value: "Geografia", label:"Geografia" },
+                { value: "História", label:"História" },
+                { value: "Matemática", label:"Matemática" },
+                { value: "Português", label:"Português" },
+                { value: "Química", label:"Química" },
+              ]}  
+            ></Select>
             <Input name="cost" label="Custo da sua hora por aula"></Input>
             
           </fieldset>
